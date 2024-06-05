7a47ca2201bf293915103f855f258aec83c99c43
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-06-05 15:59:28 +0400

    c

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,34 @@
-import { Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateDto } from './dto/create.dto';
 
 @Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('test/:id')
+  @Get('get/:id')
   getHello(@Param('id', ParseIntPipe) id: number) {
+    if (id < 1) {
+      throw new BadRequestException('error');
+    }
     // return this.appService.getHello();
     return id;
   }
 
+  @UsePipes(new ValidationPipe())
   @Post('create')
-  create() {
+  create(@Body() dto: CreateDto) {
     console.log('post');
+    return dto;
   }
 }
