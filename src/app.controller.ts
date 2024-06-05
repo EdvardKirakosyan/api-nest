ecefdd814a115a090a5b2e7d6be11fac65b02abe
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-06-05 15:17:34 +0400

    c

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('test/:id')
+  getHello(@Param('id', ParseIntPipe) id: number) {
+    // return this.appService.getHello();
+    return id;
+  }
+
+  @Post('create')
+  create() {
+    console.log('post');
   }
 }
